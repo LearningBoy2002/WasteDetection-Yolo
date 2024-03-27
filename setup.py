ffe58a062cbc002e4a34439570917db321dc9403
LearningBoy2002 <bishwarupbbiswas@gmail.com>
2024-03-27 20:26:01 +0530

    logger, exception and utils added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'LearningBoy2002',
+    author_email= 'bishwarupbbiswas@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)

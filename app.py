ad286a27f943646a530fe317896901905975135a
LearningBoy2002 <bishwarupbbiswas@gmail.com>
2024-03-28 12:45:55 +0530

    DAta ingestion added

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from wasteDetection.logger import logging
-
+from wasteDetection.pipeline.training_pipeline import TrainPipeline
 logging.info("Welcome")
+
+obj = TrainPipeline()
+obj.run_pipeline()
